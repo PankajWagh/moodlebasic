58d4d4087df9892474f38225ec7d46d7919a3360
Pankaj Wagh <prwaugh@gmail.com>
2023-01-06 16:37:00 +0530

    Color Theme changes

---
+++ makebuild.bat
@@ -0,0 +1,6 @@
+ionic cordova platform rm android
+ionic cordova platform add android@11
+ionic cordova resources --splash
+ionic cordova resources --icon
+ionic cordova resources
+ionic cordova build android --prod --release -- -- --minSdkVersion=30
